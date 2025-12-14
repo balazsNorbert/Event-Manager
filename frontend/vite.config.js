110f84ac8c43d0fd9b32298f6b3299f6ce77ea15
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2025-12-14 19:34:42 +0200

    Initial commit: Dockerized Laravel + Vue3 + Nginx + TLS setup.

---
+++ frontend/vite.config.js
@@ -0,0 +1,27 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import fs from 'fs'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  server: {
+    https: {
+      key: fs.readFileSync('./certs/localhost-key.pem'),
+      cert: fs.readFileSync('./certs/localhost.pem'),
+    },
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
