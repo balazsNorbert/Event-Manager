bcb7234194c66d3c42adea7b825d6f87ae5cf916
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2026-01-04 16:43:37 +0200

    Implement automatic logout and redirect to homepage on token expiration.

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
+import { useAuthStore } from './stores/auth';
 
 axios.defaults.baseURL = '/api';
 
 axios.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const authStore = useAuthStore();
+  const token = authStore.token;
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
@@ -11,5 +13,17 @@ axios.interceptors.request.use((config) => {
 }, (error) => {
   return Promise.reject(error);
 });
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      const authStore = useAuthStore();
+      authStore.logout();
+      window.location.href = '/';
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axios;
